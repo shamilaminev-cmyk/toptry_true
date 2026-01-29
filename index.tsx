f7830d4ff06566a33f8b1c1687e8b22c3243841e
Shamil Aminev <shamil.aminev@gmail.com>
2026-01-29 15:52:19 +0300

    Fix: resolve index.tsx merge conflict

--- index.tsx
+++ index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./index.css";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -6,6 +7,12 @@ import { patchFetchForApi } from './fetchPatch';
 patchFetchForApi();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Could not find root element to mount to');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
