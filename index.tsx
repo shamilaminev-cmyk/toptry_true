58acd1aa2554ff8ca50a8aa110ae8fd37502da2a
Shamil Aminev <shamil.aminev@gmail.com>
2026-02-10 17:16:16 +0300

    fix(web): repair index.tsx entry order (rootElement before render)

--- index.tsx
+++ index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { patchFetchForApi } from "./fetchPatch";
-patchFetchForApi();
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { patchFetchForApi } from "./fetchPatch";
 
+patchFetchForApi();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
-  throw new Error('Could not find root element to mount to');
+  throw new Error("Could not find root element to mount to");
 }
 
 ReactDOM.createRoot(rootElement).render(
